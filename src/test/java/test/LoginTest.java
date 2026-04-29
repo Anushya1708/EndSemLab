78a142820ba3acb9a2eaf7f018e0d02ea071de43
Anushya <2k23aids04@kiot.ac.in>
2026-04-29 11:37:46 +0530

    increase in test cases

--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -19,31 +19,45 @@ public class LoginTest {
         driver.manage().window().maximize();
     }
 
-    @Test
-    public void loginTest() throws InterruptedException {
+    @DataProvider(name = "loginData")
+    public Object[][] getData() {
+        return new Object[][]{
+            {"Geetha_Kumari", "geetha12345"},      // valid
+            {"Geetha_kumari", "1234567890"},  // valid (create in site)
+            {"wronguser", "test123"},    // invalid
+            {"testuser", "wrongpass"}    // invalid
+        };
+    }
+
+    @Test(dataProvider = "loginData")
+    public void loginTest(String username, String password) throws InterruptedException {
 
         // Click Login button
         driver.findElement(By.id("login2")).click();
         Thread.sleep(2000);
 
         // Enter Username
-        driver.findElement(By.id("loginusername")).sendKeys("Geetha_Kumari");
+        driver.findElement(By.id("loginusername")).clear();
+        driver.findElement(By.id("loginusername")).sendKeys(username);
 
         // Enter Password
-        driver.findElement(By.id("loginpassword")).sendKeys("geetha12345");
+        driver.findElement(By.id("loginpassword")).clear();
+        driver.findElement(By.id("loginpassword")).sendKeys(password);
 
         // Click Login
         driver.findElement(By.xpath("//button[text()='Log in']")).click();
 
         Thread.sleep(3000);
 
-        // Verify login success (username displayed)
-        WebElement user = driver.findElement(By.id("nameofuser"));
-
-        if(user.isDisplayed()) {
-            System.out.println("Login is Successful");
-        } else {
-            System.out.println("Login is Failed");
+        // Check success
+        try {
+            WebElement user = driver.findElement(By.id("nameofuser"));
+            if (user.isDisplayed()) {
+                System.out.println("Login Successful for: " + username);
+                driver.findElement(By.id("logout2")).click(); // logout for next test
+            }
+        } catch (Exception e) {
+            System.out.println("Login Failed for: " + username);
         }
     }
 
